9f8df3edbf836c26c2d21f50ca29f24709600f7b
arcpato <AngelicaRosenzweig@Angelicas-MacBook-Air.local>
2015-11-01 11:05:59 -0800

    probando forma contacto

---
+++ dist/js/contactform.js
@@ -0,0 +1,30 @@
+var contactForm = contactSection.querySelector('form'),
+    inputName = contactForm.querySelector('[name="name"]'),
+    inputEmail = contactForm.querySelector('[name="contact"]'),
+    textAreaMessage = contactForm.querySelector('[name="message"]'),
+    sendButton = contactForm.querySelector('[name="submit"]');
+
+    sendButton.addEventListener('click', function(event){
+      event.preventDefault(); // prevent the form to do the post.
+
+      sendButton.innerHTML = 'sending..';
+
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', '//formspree.io/arcpato@gmail.com', true);
+      xhr.setRequestHeader("Accept", "application/json")
+      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+
+      xhr.send(
+        "name=" + inputName.value +
+        "&email=" + inputEmail.value +
+        "&message=" + textAreaMessage.value);
+
+      xhr.onloadend = function (res) {
+        if (res.target.status === 200){
+          sendButton.innerHTML = 'Message sent!';
+        }
+        else {
+          sendButton.innerHTML = 'Error!';
+        }
+      }
+    });
